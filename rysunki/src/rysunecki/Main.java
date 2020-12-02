f3a9115884038d9a5dcc985d4fb8ec9b5e48bbd1
MrPioCze <MrPioCze@users.noreply.github.com>
2020-12-02 11:49:39 +0100

    Update Main.java
    + Pi via Monte Carlo method

--- rysunki/src/rysunecki/Main.java
+++ rysunki/src/rysunecki/Main.java
@@ -6,7 +6,10 @@ public class Main {
 
 	  public static void main(String[] args) {
 		  int w = 900; //30 * 30
-		  int e = 144; //12 * 12
+		  int e = 144; //12 * 12 //promien
+		  double pi;
+		  int suma1 = 0;
+		  int total = 50000;
 
 		  StdDraw.setCanvasSize(w, w);
 		  StdDraw.setScale(-w / 2, w / 2);
@@ -14,17 +17,23 @@ public class Main {
           StdDraw.setPenRadius(0.05);
           
           Random r = new Random();
-          for (int i = 0; i < 50000; i++) {
+          for (int i = 0; i <= total; i++) {
         	  int x = r.nextInt(w) - w / 2;
         	  int y = r.nextInt(w) - w / 2;        	  
         	 if (x * x + y * y < e * e) {
         		 StdDraw.setPenColor(StdDraw.DARK_GRAY);
+        		 suma1++;
         	 }
         	 else {
         		 StdDraw.setPenColor(StdDraw.CYAN);
-        	 }
+        		 }
         	 StdDraw.point(x, y);
-          }
+        	 if (x * x + y * y <= 1) {
+        		pi = 4. * (suma1 / total);
+        		System.out.println(pi);
+        	 }
+          } 
+         
           
           
       }
